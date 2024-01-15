9062d4b7516b22681302323a71c1d2837fe67ef3
Arthur Bacci <arthurbacci@protonmail.com>
2024-01-15 19:16:15 -0300

    refactor(errno): separation into small functions

--- errno/errno.c
+++ errno/errno.c
@@ -45,43 +45,47 @@ void print_errno(size_t i) {
 		errnos_data[i].s, errnos_data[i].n, strerror(errnos_data[i].n)
 	);
 }
+
+void print_all_errnos() {
+	size_t i;
+	for (i = 0; errnos_data[i].s; i++)
+		print_errno(i);
+}
+
+void print_n_errno(int number) {
+	size_t i;
+	/* TODO: maybe take advantage of the list being 
+	 * sorted? */
+
+	for (i = 0; errnos_data[i].s; i++)
+		if (errnos_data[i].n == number)
+			print_errno(i);
+}
+
+void print_s_errno(char *s) {
+	size_t i;
+	/* break is fine since there are no duplicate numbers
+	 * for a name. */
+	for (i = 0; errnos_data[i].s; i++)
+		if (0 == strcmp(errnos_data[i].s, s)) {
+			print_errno(i);
+			break;
+		}
+}
+
 int main(int argc, char **argv) {
 	/* setlocale(LC_ALL, ""); */
 
+
 	if (argc == 1) {
-		size_t i;
-		for (i = 0; errnos_data[i].s; i++)
-			print_errno(i);
+		print_all_errnos();
 	} else if (argc == 2) {
-		/* boolean */
-		int is_number = 1;
 		size_t i;
-		for (i = 0; argv[1][i]; i++) {
-			if (!isdigit(argv[1][i])) {
-				is_number = 0;
-				break;
-			}
-		}
+		/* if argv[1][i] is NULL, all the characters are digits */
+		for (i = 0; argv[1][i] && isdigit(argv[1][i]); i++);
 
-		if (is_number) {
-			int number = atoi(argv[1]);
-			size_t i;
-			/* TODO: maybe take advantage of the list being 
-			 * sorted? */
-
-			for (i = 0; errnos_data[i].s; i++)
-				if (errnos_data[i].n == number)
-					print_errno(i);
-		} else {
-			/* break is fine since there are no duplicate numbers
-			 * for a name. */
-			for (i = 0; errnos_data[i].s; i++) {
-				if (0 == strcmp(errnos_data[i].s, argv[1])) {
-					print_errno(i);
-					break;
-				}
-			}
-		}
+		if (argv[1][i]) print_s_errno(argv[1]);
+		else            print_n_errno(atoi(argv[1]));
 	} else {
 		usage();
 	}
